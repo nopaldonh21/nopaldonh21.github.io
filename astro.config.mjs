9b6850d92b55fa6cc921c2a19826a0c16f99a8db
nopaldonh <nopaldonh21@gmail.com>
2023-03-30 20:16:05 +0700

    upd: site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-portfolio-uzair.vercel.app",
+  site: "https://nopaldonh21.github.io",
   integrations: [
     tailwind(),
     sitemap({
