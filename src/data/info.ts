c5f0aef18362b2e90494c9e9d3b777a0d82361c6
nopaldonh <nopaldonh21@gmail.com>
2023-03-30 22:13:53 +0700

    upd: data info, hero and footer

--- src/data/info.ts
+++ src/data/info.ts
@@ -1,88 +1,53 @@
 export const info = {
-  baseUrl: "https://astro-portfolio-uzair.vercel.app",
-  name: "Uzair Ahmed",
+  baseUrl: "https://nopaldonh21.github.io",
+  name: "Nopaldo Nur Hidayat",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Karachi, Pakistan.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  about: `I'm a web developer based in Megamendung, Kabupaten Bogor, Jawa Barat, Indonesia.
+    I am interested in programming, especially web and application development, keep up with technology developments, and learn about it.
+    I am able to communicate well and can work in a team or individually.
+    I master several technologies or programming languages such as HTML, CSS, JavaScript, Vue.js, React, PHP, and Laravel.
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
-      endDate: "Present",
+      name: "CV VICTORYSTUDIO09 (Full Stack Developer - Intern)",
+      location: "Kota Bogor, Jawa Barat, Indonesia",
+      startDate: "Dec 2021",
+      endDate: "Mar 2022",
       description: [
-        "- Developing screens and UI components for the web application using React and Tailwind.",
-        "- Fixing UI issues and integrating backend APIs with Redux Saga.",
+        "- Create a laravel-based pdf report generation application",
+        "- Developing the Victory Studio corporate website",
+        "- Create a simple web for promotion, information and sales",
       ],
     },
 
     {
-      name: "Coupon Approve (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
+      name: "CV IDS KEJAR.ID (Frontend Developer - Intern)",
+      location: "Kota Bogor, Jawa Barat, Indonesia",
+      startDate: "Mar 2021",
+      endDate: "Jun 2021",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
-      ],
-    },
-
-    {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
-      description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "Developing a Laravel-based learning application called kejar.id, creating a Concept Map and CRUD page for the teacher's menu",
       ],
     },
   ],
 
   education: [
     {
-      name: "Pakistan Institute of Professional Science & Technology (PIPS-TECH)",
-      location: "Karāchi, Sindh, Pakistan",
+      name: "SMK Wikrama Bogor",
+      location: "Kota Bogor, Jawa Barat, Indonesia",
       startDate: "2019",
       endDate: "2022",
       description: [
-        "Diploma of Associate Engineering,  Computer Information Technology (CIT)",
+        "Rekayasa Perangkat Lunak (Software Engineering)",
       ],
-    },
-    {
-      name: "Govt. Degree Boys College Buffer Zone",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2018",
-      endDate: "2020",
-      description: ["Intermediate, Pre-Engineering"],
-    },
-    {
-      name: "Ghazi Foundation Schools & Colleges",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "2016",
-      endDate: "2018",
-      description: ["Matriculation,  Computer Science"],
     },
   ],
 
   socialMedia: {
-    facebook: "https://www.facebook.com/profile.php?id=100022899849583",
-    twitter: "https://twitter.com/Uzzii_21",
-    github: "https://github.com/uzzii-21",
-    email: "uzairahmed@74372gmail.com",
-    linkedin: "https://www.linkedin.com/in/uzzii21/",
+    email: "nopaldonh@gmail.com",
+    linkedin: "https://www.linkedin.com/in/nopaldo-nur-hidayat/",
+    github: "https://github.com/nopaldonh21",
   },
 
   projects: [
